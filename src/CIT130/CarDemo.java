daa8383a0348f3e0d4d3431af96a66fae53e3f8e
Johnny Cheng <johnnychengbustamante@gmail.com>
2024-02-28 20:59:41 -0500

    added main

--- src/CIT130/CarDemo.java
+++ src/CIT130/CarDemo.java
@@ -6,6 +6,18 @@ public class CarDemo {
         System.out.println(car.toString());
         System.out.printf("For my European friends, in Kilometers it would be a top speed of %.2f Kilometers per hour and " +
                 "it would give me %.2f Kilometers per gallon.\n", car.getMaxKPH(), car.getKPG());
-        System.out.printf("If I add nitrous oxide to the car, it would increase the max speed by 10 miles per hour and the max speed would be %.2f miles per hour.", car.getMaxMPH() + car.increaseSpeedBy10());
+        System.out.printf("If I add nitrous oxide to the car, it would increase the max speed by 10 miles per hour and the max speed would be %.2f miles per hour.\n", car.getMaxMPH() + car.increaseSpeedBy10());
+
+        System.out.println();
+        CarClient car2 = new CarClient("Honda", "Accord");
+        System.out.println(car2.toString());
+
+        System.out.println();
+        CarClient car3 = new CarClient("Ford", "Focus", "Blue");
+        System.out.println(car3.toString());
+
+        System.out.println();
+        CarClient car4 = new CarClient("Jeep", "Wrangler", "Black", 2015);
+        System.out.println(car4.toString());
     }
 }
