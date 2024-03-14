533be72ec170177ab51a5f9309aeca86b152775f
Johnny Cheng <johnnychengbustamante@gmail.com>
2024-03-14 16:45:33 -0400

    houseClass

---
+++ src/CIT130/HouseListing.java
@@ -0,0 +1,127 @@
+package CIT130;
+//Name: Johnny J. Cheng
+// Regarding above error - Either leave package blank OR ensure BOTH files belong to the same package
+
+
+import java.text.CharacterIterator;
+import java.text.DecimalFormat; // YOU MAY NEED THIS IN the toString() METHOD
+import java.text.StringCharacterIterator;
+
+/**
+ * Complete the code below the numbered comments, 1 - 4.
+ * Do not change the pre-written code
+ * Exam 2
+ * @author
+ */
+public class HouseListing
+{
+    /*
+     * 1. Create instance variables as shown in UML
+     */
+    private String listingNumber;
+    private String houseDescription;
+    private double listingPrice;
+
+
+    /*
+     * default constructor (If an empty house object is created)
+     * assigns listingNumber the text string 0000
+     * assigns houseDescription an empty text string
+     * assigns listingPrice the value of 1.0
+     */
+    public HouseListing( )
+    {
+        listingNumber = "0000";
+        houseDescription = "";
+        listingPrice = 1.0;
+    }
+    /*
+     * 2.
+     * Overloaded Constructor
+     * call the method setListingNumber() passing listCode
+     * assigns houseDescription the value style holds
+     * call the method setPrice() passing price
+     */
+    public HouseListing(String listCode, String style, double price)
+    {
+    setListingNumber(listCode);
+    houseDescription = style;
+    setPrice(price);
+    }
+    /*
+     * accessor method
+     * returns listingNumber
+     */
+    public String getHouseListing( )
+    {
+        return listingNumber;
+    }
+    /*
+     * accessor method
+     * returns houseDescription
+     */
+    public String getHouseDescription( )
+    {
+        return houseDescription;
+    }
+    /*
+     * accessor method
+     * returns listingPrice
+     */
+    public double getListPrice( )
+    {
+        return listingPrice;
+    }
+    /* 3.
+     * mutator method
+     * parameter changedNumber
+     * method checks the number of characters in the parameter
+     * if the number of characters is less than 4 assign
+     * listingNumber a string of 4 zeros, otherwise assign
+     * listingNumber the value of the parameteer
+     */
+    public void setListingNumber(String changedNumber) {
+        if (changedNumber.length() < 4) {
+            listingNumber = "0000";
+        } else {
+            listingNumber = changedNumber;
+        }
+    }
+    /*
+     * mutator method
+     * parameter changeStyle
+     * assigns houseDescription the parameter value
+     */
+    public void setHouseDescription(String changeStyle)
+    {
+        houseDescription = changeStyle;
+    }
+
+    /* 4.
+     * mutator method
+     * parameter changePrice
+     * method checks the parameter's value if the value is zero
+     * or less assign listingPrice one dollar, otherwise
+     * assign listingPrice the parameter value
+     */
+    public void setPrice(double changedPrice)
+    {
+    if(changedPrice <= 0){
+        listingPrice = 1;
+    }else{
+        listingPrice = changedPrice;
+    }
+    }
+    /* 5.
+     * this method displays a formatted text string with the values the
+     * instance variables hold. See example of output for formatting'
+     * think, decimal formatting, or printf
+     */
+    @Override
+    public String toString( )
+    {
+        return String.format("Listing Number: %s, Description: %s, Listing Price: $%,.2f", listingNumber, houseDescription, listingPrice); // written for syntax only, an error otherwise
+    }
+
+
+}
---
+++ src/CIT130/HouseListingDemo.java
@@ -0,0 +1,105 @@
+package CIT130;
+
+// Regarding above error - Either leave package blank OR ensure BOTH files belong to the same package
+
+import java.util.Scanner;
+
+
+/**
+ * This program will use the HouseListing class and display a list of
+ * houses sorted by the house's listing number
+ *
+ * Complete the code below the numbered comments, 1 - 4. DO NOT CHANGE the
+ * pre-written code
+ * Exam 2
+ * @author
+ *
+ */
+
+public class HouseListingDemo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        HouseListing[] list;
+        String listNumber, listDesc;
+        int count = 0;
+        double listPrice;
+        String output;
+
+        double mostExpensive;
+
+        do{
+            System.out.print("Enter the number of houses --> ");
+            count = scan.nextInt();
+        }while(count < 1);
+
+        /* 1.
+         * create an array of HouseListing objects using the variable that holds
+         * the user's response to the prompt above
+         */
+        HouseListing [] HlObjects = new HouseListing [count];
+
+        // for loop loads the array with HouseListing's constructor
+        for (int i = 0; i < HlObjects.length; i++){
+            System.out.println("\n***HOUSE " + (i+1) + "***");
+            // Prompt for house listing #
+            System.out.print("Enter house listing " +
+                    "number (Alphanumeric) #"+ (i+1) +": ");
+            listNumber = scan.next();
+
+            // Clear buffer
+            scan.nextLine();
+
+            // Prompt for house description
+            System.out.print("Enter description for " +
+                    "house #" +(i+1) + ": ");
+            listDesc = scan.nextLine();
+
+            // Prompt for house price
+            System.out.print("Enter listing price for "
+                    + "house #" +(i+1) + ": ");
+            listPrice = scan.nextDouble();
+
+            /* 2.
+             * create a HouseListing object using the input values and store/load
+             * the object in the array
+             */
+            HlObjects[i] = new HouseListing(listNumber, listDesc, listPrice);
+        }
+
+
+        /* 3.
+         * Assign the 0th element of the array to the most expensive house
+           think ... you can't assign an object to a price but the object
+           has a method that may help you here ...
+
+           ***If you decide to complete this step a different way (or perhaps not at all?)
+           Please write a comment explaining how / why.***
+         */
+
+        mostExpensive = HlObjects[0].getListPrice();
+        for(int i = 1; i < HlObjects.length; i++){
+            if(HlObjects[i].getListPrice() > mostExpensive){
+                mostExpensive = HlObjects[i].getListPrice();
+            }
+        }
+
+
+        output = "Listings:\n";
+        /*
+         * loop below builds the output string
+         */
+        for (int i = 0; i < HlObjects.length; i++) {
+            output += HlObjects[i] + "\n";
+
+            /* 4.
+              Using a control structure, find and the store the double varaible
+              which holds the most expensive house (We don't need to listing #)
+              JUST THE PRICE
+            */
+        }
+        // output
+        System.out.println(output);
+        System.out.printf("The most expensive house on the market costs: $%,.2f",mostExpensive);
+    }
+}
